982bc6ad86f46340f0b844fffdba384db2a4b24f
Justin <jumoell@protonmail.com>
2024-10-17 17:11:11 -0500

    chore: upgrade to v0.4.3 working

--- scripts/dev/mprocs/run.sh
+++ scripts/dev/mprocs/run.sh
@@ -8,4 +8,6 @@ ensure_in_dev_shell
 build_workspace
 add_target_dir_to_path
 
-../fedimint/target-nix/debug/devimint --link-test-dir .../fedimint/target/devimint "$@" dev-fed --exec bash -c 'mprocs -c misc/mprocs.yaml 2>$FM_LOGS_DIR/devimint-outer.log'
+export FM_REL_NOTES_ACK=0_4_xyz
+
+devimint --link-test-dir ./target/devimint "$@" dev-fed --exec bash -c 'mprocs -c misc/mprocs.yaml 2>$FM_LOGS_DIR/devimint-outer.log'
