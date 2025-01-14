81e07a5ffabca937f56e1a55d6e9d986adad9613
Justin Moeller <jumoell@protonmail.com>
2025-01-14 14:01:28 -0600

    chore: add packages

--- flake.nix
+++ flake.nix
@@ -12,7 +12,13 @@
       in {
         devShells = {
           # You can expose all or specific shells from the original flake
-          default = devShells.default;
+          default = devShells.default.overrideAttrs (old: {
+            nativeBuildInputs = old.nativeBuildInputs or [] ++ [
+              fedimint.packages.${system}.devimint
+              fedimint.packages.${system}.gateway-pkgs
+              fedimint.packages.${system}.fedimint-pkgs
+            ];
+          });
         };
       }
     );
