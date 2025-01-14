b300875d6b4b6ff4224269570554c996b4a05acb
Justin Moeller <jumoell@protonmail.com>
2025-01-14 12:00:23 -0600

    chore: remove nix files, tests are still commented

--- nix/overlays/cargo-honggfuzz.nix
+++
@@ -1,5 +0,0 @@
-final: prev: {
-  cargo-hongfuzz = prev.callPackage ../pkgs/cargo-honggfuzz.nix {
-    inherit (prev.darwin.apple_sdk.frameworks) Security;
-  };
-}
--- nix/overlays/esplora-electrs.nix
+++
@@ -1,5 +0,0 @@
-final: prev: {
-  esplora-electrs = prev.callPackage ../pkgs/esplora-electrs.nix {
-    inherit (prev.darwin.apple_sdk.frameworks) Security;
-  };
-}
