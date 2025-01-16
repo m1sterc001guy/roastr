de60b570cc5aa7d37ffd12a6c2dc61342f33b8d5
Justin Moeller <jumoell@protonmail.com>
2025-01-16 12:23:09 -0600

    test: test can run one command

--- roastr-tests/bin/tests.rs
+++ roastr-tests/bin/tests.rs
@@ -1,7 +1,29 @@
+use devimint::cmd;
+use tracing::info;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     devimint::run_devfed_test(|dev_fed, _process_mgr| async move {
-        tracing::info!("roastr running devimint test");
+        info!("roastr guardian 0 creating note...");
+
+        let client0 = dev_fed.fed().await?.new_joined_client("guardian0").await?;
+        let event_id = cmd!(
+            client0,
+            "--our-id",
+            "0",
+            "--password",
+            "pass",
+            "module",
+            "roastr",
+            "create-note",
+            "--text",
+            "ROASTR"
+        )
+        .out_json()
+        .await?["event_id"]
+            .to_string();
+
+        info!(?event_id, "EventID");
         Ok(())
     })
     .await
